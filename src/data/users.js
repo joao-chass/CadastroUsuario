2921f388c8493f324de735731edf7c115d70fe2d
joao-chass <joaoborges07@hotmail.com>
2020-12-16 18:09:14 -0300

    Primeiro commit

---
+++ src/data/users.js
@@ -0,0 +1,34 @@
+export default [
+    {
+        id: 1,
+        phone: '944587521',
+        name: 'Maria Silva',
+        email: 'maria@silcva.com',
+        avatarUrl: 
+        'https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png'
+    },
+    {
+        id: 2,
+        phone: '944587521',
+        name: 'Joao Pereira',
+        email: 'joao@pereira.com',
+        avatarUrl: 
+        'https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png'
+    },
+    {
+        id: 3,
+        phone: '944587521',
+        name: 'Fernanda gomes',
+        email: 'maria@silcva.com',
+        avatarUrl: 
+        'https://cdn.pixabay.com/photo/2014/04/03/10/32/user-310807_960_720.png'
+    },
+    {
+        id: 4,
+        phone: '944587521',
+        name: 'Pedro dos Santos',
+        email: 'maria@silcva.com',
+        avatarUrl: 
+        'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png'
+    },
+]
